144e49cbf9a9c18416990ae811d6a1122801c40b
TSW <teamstormwork@gmail.com>
2018-09-26 17:10:42 +0100

    Test

--- TimerApp/src/app/app.module.ts
+++ TimerApp/src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { CountdownEntityComponent } from './countdown-entity/countdown-entity.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountdownEntityComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
