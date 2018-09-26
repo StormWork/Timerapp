1948c0a8b1357fe187671faed2f878763f3929f9
TSW <teamstormwork@gmail.com>
2018-09-26 17:11:13 +0100

    Test

---
+++ TimerApp/src/app/countdown-entity/countdown-entity.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'countdown-entity',
+  templateUrl: './countdown-entity.component.html',
+  styleUrls: ['./countdown-entity.component.css']
+})
+export class CountdownEntityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
